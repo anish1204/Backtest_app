bd57eeb84f6bb057a6aa88e5372fd129e99ff55d
Anish Patil <anishpatil304@gmail.com>
2025-10-24 00:23:30 +0530

    fixing yahoo api issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import companies, prices, fundamentals, strategies, backtest
+import requests
+
 
 app = FastAPI()
 
@@ -17,3 +19,12 @@ app.include_router(prices.router)
 app.include_router(fundamentals.router)
 app.include_router(strategies.router)
 app.include_router(backtest.router)
+
+
+@app.get("/test-yf")
+def test_yf():
+    try:
+        r = requests.get("https://query1.finance.yahoo.com/v8/finance/chart/AAPL")
+        return {"status": r.status_code, "length": len(r.text)}
+    except Exception as e:
+        return {"error": str(e)}
--- backtest_frontend/next.config.js
+++ backtest_frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",           // anything under /api/
+        destination: "https://trademo.onrender.com", // your backend
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
